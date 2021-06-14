a75e84361ca10156a58aa3ec33cc2ab0bc859560
Brant <bdm@brant.work>
2021-06-14 14:14:05 -0500

    Added Server Index File

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const createCheckoutSession = require('./checkout')
+
+if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config({path: './.env'})
+}
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(express.json())
+app.use(cors({origin: true}))
+
+app.get('/', (req, res) => res.send('Hello Express'))
+
+app.post('/create-checkout-session', createCheckoutSession)
+
+app.listen(port, () => console.log(`server listening on port: ${port}`))
